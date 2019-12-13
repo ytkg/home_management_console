5d8c3bbf73d78f00a4f38fddab3a21a60461d548
Yoshiki Takagi <takagi.yoshiki@adways.net>
2019-12-13 13:56:38 +0900

    [add] 消耗品一覧ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/light_on'
   get 'home/light_off'
+  resources :items, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
