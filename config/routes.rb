6626512f3a2dfe4e09202090071bfcc7040b188c
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2020-06-17 02:24:18 +0900

    [update] Rails Upgrade 5.2.4.3 -> 6.0.3.1

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'home/light_off'
   resources :items, only: [:index]
   resources :stocks, only: [:index, :new, :edit, :update, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
